ee9161d5ebb67190c6fa8e313322a0013a3e4fe0
Taiki Abe <hiyu@MacBook-Air.local>
2025-05-26 18:09:47 +0900

    Myページの作成

---
+++ app/models/post.rb
@@ -0,0 +1,24 @@
+class Post < ApplicationRecord
+  validates :shop_name, presence: true
+  #validates :visit_date, presence: true
+  #validates :category_id, presence: true
+  #validates :feeling_id, presence: true
+  #validates :companion_id, presence: true
+  #validates :visit_reason_id, presence: true
+  #validates :prefecture_id, presence: true
+  #validates :city_id, presence: true
+
+  belongs_to :user
+  #belongs_to :category
+  #belongs_to :feeling
+  #belongs_to :companion
+  #belongs_to :visit_reason
+  #belongs_to :prefecture
+  #belongs_to :city
+
+  #has_many :favorites, dependent: :destroy
+  #has_many :favorited_users, through: :favorites, source: :user
+
+  #has_many :visits, dependent: :destroy
+  #has_many :calendar_records, dependent: :destroy
+end
