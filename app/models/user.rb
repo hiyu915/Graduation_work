de213000138300062b4899259acba96ae35179af
hiyu915 <hiyuhiyu915@gmail.com>
2025-05-26 18:17:54 +0900

    Lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || password.present? }
   validates :password, confirmation: true, if: -> { new_record? || password.present? }
   validates :password_confirmation, presence: true, if: -> { new_record? || password.present? }
-  
+
   has_many :posts, dependent: :destroy
-  #has_many :favorites, dependent: :destroy
-  #has_many :favorite_posts, through: :favorites, source: :post
-  #has_many :visits, dependent: :destroy
+  # has_many :favorites, dependent: :destroy
+  # has_many :favorite_posts, through: :favorites, source: :post
+  # has_many :visits, dependent: :destroy
 end
