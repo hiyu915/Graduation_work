6ee76a38ac124c6394640d9a81acee5487e09b7e
hiyu915 <hiyuhiyu915@gmail.com>
2025-05-30 16:40:48 +0900

    bin/render-build.sh修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,22 +1,8 @@
-#!/usr/bin/env bash
 set -o errexit
 
-echo "Terminating existing database connections..."
-psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'repilog' AND pid <> pg_backend_pid();"
-
-echo "Running bundle install..."
 bundle install
-
-echo "Cleaning assets..."
-bin/rails assets:clobber
-
-echo "Precompiling assets..."
-bin/rails assets:precompile
-
-echo "Resetting and migrating database..."
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:migrate:reset
-
-echo "Seeding database..."
-bin/rails db:seed
-
-echo "Build script completed."
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+rails db:seed
