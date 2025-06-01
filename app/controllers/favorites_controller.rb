67b91a8d896fa512a07ae3e57fb504860a30f8df
hiyu915 <hiyuhiyu915@gmail.com>
2025-06-02 01:24:53 +0900

    いいねのAjax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,27 @@
 class FavoritesController < ApplicationController
   before_action :require_login
-
+  before_action :set_post
+  
   def create
-    post = Post.find(params[:post_id])
-    current_user.favorites.create(post: post)
-    redirect_to request.referer || root_path
+    current_user.favorites.create(post: @post)
+    respond_to do |format|
+      format.html { redirect_to request.referer || root_path }
+      format.turbo_stream
+    end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite&.destroy
-    redirect_to request.referer || root_path
+    respond_to do |format|
+      format.html { redirect_to request.referer || root_path }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
