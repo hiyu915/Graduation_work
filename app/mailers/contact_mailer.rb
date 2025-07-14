a400791ab47253c595635eb33579d6c4f5f713b1
hiyu915 <hiyuhiyu915@gmail.com>
2025-07-15 02:28:58 +0900

    お問い合わせ機能作成

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,9 +3,13 @@ class ContactMailer < ApplicationMailer
 
   def contact_email(contact_form)
     @category = contact_form.category
-    @email = contact_form.email
     @message = contact_form.message
+    @user_email = contact_form.email
 
-    mail(from: @email, subject: "お問い合わせがありました")
+    mail(
+      from: "hiyuhiyu915@gmail.com",
+      reply_to: @user_email,
+      subject: "お問い合わせがありました"
+    )
   end
 end
