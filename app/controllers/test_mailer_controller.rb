7a4b0a4608fdfa34396d66e2c49894c230bb8286
hiyu915 <hiyuhiyu915@gmail.com>
2025-08-04 05:29:17 +0900

    エラー対応

--- app/controllers/test_mailer_controller.rb
+++ app/controllers/test_mailer_controller.rb
@@ -1,4 +1,6 @@
 class TestMailerController < ApplicationController
+  skip_before_action :require_login
+
   def send_test
     user = User.last
     UserMailer.activation_needed_email(user).deliver_now
