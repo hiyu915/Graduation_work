ef305c1265602b6df1d86926e19ed3d53ba4886e
hiyu915 <hiyuhiyu915@gmail.com>
2025-09-08 09:29:20 +0900

    静的OGP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,31 @@ module ApplicationHelper
     base_title = "リピログ"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
+
+  def default_meta_tags(page_title_text = nil)
+    {
+      site: 'リピログ',
+      title: page_title_text || 'リピログ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'リピログは、「また行きたい」と思ったお店を中心に記録・整理できるグルメ記録アプリです。',
+      keywords: 'グルメ, 記録, 食事, リピート',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: page_title_text || 'リピログ',
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@hiyuRUNTEQ',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
