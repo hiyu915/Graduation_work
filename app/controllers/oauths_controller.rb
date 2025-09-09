73813f89c5379061fc0d4a941e8aaed1d2d4e011
hiyu915 <hiyuhiyu915@gmail.com>
2025-09-09 18:15:27 +0900

    日本語対応

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,22 +1,21 @@
 class OauthsController < ApplicationController
-skip_before_action :require_login
+  skip_before_action :require_login
+
   def oauth
-    # 指定されたプロバイダの認証ページにユーザーをリダイレクトさせる
     login_at(auth_params[:provider])
   end
 
   def callback
     provider = auth_params[:provider]
-    # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
+
     if (@user = login_from(provider))
-      redirect_to root_path, notice: "#{provider.titleize}アカウントでログインしました"
+      redirect_to root_path, notice: t("oauths.callback.success", provider: provider.titleize)
     else
       begin
-        # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
         signup_and_login(provider)
-        redirect_to root_path, notice: "#{provider.titleize}アカウントでログインしました"
+        redirect_to root_path, notice: t("oauths.callback.success", provider: provider.titleize)
       rescue
-        redirect_to root_path, alert: "#{provider.titleize}アカウントでのログインに失敗しました"
+        redirect_to root_path, alert: t("oauths.callback.failure", provider: provider.titleize)
       end
     end
   end
