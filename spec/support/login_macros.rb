9ca4a81d27bcbe37140bb8d4c4e97c69d8cb3455
hiyu915 <hiyuhiyu915@gmail.com>
2025-09-13 00:08:30 +0900

    リクエストテスト準備

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,20 @@
 module LoginMacros
+  # SystemSpec用のログインメソッド
   def login(user)
     visit login_path
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
     expect(page).to have_content('ログアウト')
+  end
+  
+  # RequestSpec用のログインメソッド
+  def login_as(user)
+    post login_path, params: {
+          email: user.email,
+          password: "password"
+        }
+    expect(response).to have_http_status(302)
+    expect(response.location).not_to include('login')
   end
 end
