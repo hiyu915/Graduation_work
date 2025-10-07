6e6269bc118fec7190923c2f5a77190be3dc427f
hiyu915 <hiyuhiyu915@gmail.com>
2025-10-07 19:35:44 +0900

    AWS_S3実装

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"],
-      #path_style: true
+      region: ENV["AWS_REGION"]
+      # path_style: true
     }
   else
     config.storage = :file
