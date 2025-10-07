3f6fb0b0e0f2d35c4f015ae1f5c7dc101a963881
hiyu915 <hiyuhiyu915@gmail.com>
2025-10-07 19:41:05 +0900

    AWS_S3実装

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,7 @@
-if Rails.env.production?
-  storage :fog
-else
-  storage :file
+class AvatarUploader < CarrierWave::Uploader::Base
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 end
