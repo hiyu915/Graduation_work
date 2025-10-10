cb7a29e24916ab7eb794cfc00c63ec6d01155c77
hiyu915 <hiyuhiyu915@gmail.com>
2025-10-10 23:53:32 +0900

    テスト

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-    region: ENV["S3_REGION"]
-  }
-  config.fog_directory  = ENV["S3_BUCKET_NAME"]
-  config.fog_public = false
-
   if Rails.env.development? || Rails.env.production?
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+      region: ENV["S3_REGION"]
+    }
+    config.fog_directory  = ENV["S3_BUCKET_NAME"]
+    config.fog_public = false
     config.storage = :fog
   else
     config.storage = :file
